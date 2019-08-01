009a7abf3b5a3b5bcd74bce687f4bf664a660184
Yoga Setiawan <mryoga1995@gmail.com>
2019-08-01 09:35:09 +0700

    Soal No 2 finish

--- Soal No 2.go
+++ Soal No 2.go
@@ -2,12 +2,39 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"sort"
+	"strings"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "ok")
-	})
+	router.GET("/:string", sortTextApi)
 	router.Run(":3000")
+}
+
+func sortTextApi(c *gin.Context) {
+	var hidup []string
+	var mati []string
+	inputParam := c.Param("string")
+	splitString := strings.Split(inputParam, "")
+
+	for i := 0; i < len(splitString); i++ {
+		if "a" == splitString[i] || "i" == splitString[i] || "u" == splitString[i] ||
+			"e" == splitString[i] || "o" == splitString[i] {
+			hidup = append(hidup, splitString[i])
+		} else if " " != splitString[i] {
+			mati = append(mati, splitString[i])
+		}
+	}
+
+	sort.Strings(hidup)
+	sort.Strings(mati)
+
+	result := gin.H{
+		"input": inputParam,
+		"hasil": strings.Join(hidup, "") + strings.Join(mati, ""),
+	}
+	c.JSON(http.StatusOK, result)
+
 }
